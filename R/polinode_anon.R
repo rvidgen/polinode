6412c9109e5b4920976dd64421a0991ece62caaf
Richard Vidgen <richard@vidgen.com>
2016-04-25 14:26:22 +1000

    Update polinode_anon.R

--- R/polinode_anon.R
+++ R/polinode_anon.R
@@ -4,74 +4,68 @@
 # date: 24 April 2016
 ###############################################
 
-# set directory to where the SURVEY download file is
-setwd("/Users/richardvidgen/Dropbox/_RICHARD/RdataRV/Polinode")
+# set directory to where your SURVEY download file is
+setwd("/Users/ ... /PolinodeData")
 getwd()
 
 # set input and ouput excel file names
-infile = "UNSW lecture test 4.xlsx"
-outfile = "networkout.xlsx"
+infile = "polinodesurvey.xlsx" # name of file downloaded from polinode survey
+outfile = "networkout.xlsx" # name of file to upload to polinode network
 
 # set node and edge sheet names
 nodesheet = "Node Attributes" # this is the same for all excel survey downloads
-edgesheet = "Relationship Question 1" # this changes and there can be more than one to choose from
+edgesheet = "Relationship Question 1" # this changes and there can be more than one to choose from in a survey
 
 library(xlsx)
 library(plyr)
 library(random)
 
 ##
-# no further editing of script required from here on
+# no further editing of script should be required from here on
 ##
 
 # read the nodes
 nodes <- read.xlsx(infile, sheetName=nodesheet, as.data.frame=TRUE, stringsAsFactors=F)
-nodes
+head(nodes)
 
 # generate unique random codes
 rand = randomStrings(n=nrow(nodes), len=5, digits=TRUE, upperalpha=TRUE,
                      loweralpha=TRUE, unique=TRUE, check=TRUE)
 str(rand)
 nodes$uniqueid = as.vector(rand)
-nodes
-str(nodes)
+head(nodes)
 
 # make a key list
 keylist = subset(nodes, select=c(Name, uniqueid))
-keylist
+head(keylist)
 
 # read edges
 edges <- read.xlsx(infile, sheetName=edgesheet, as.data.frame=TRUE, stringsAsFactors=F)
-edges
+head(edges)
 
 # merge random code for Source
 edges = merge(edges, nodes, by.x=c("Source"), by.y=c("Name"))
-edges
+head(edges)
 
 # merge random code for Target
 edges = merge(edges, nodes, by.x=c("Target"), by.y=c("Name"))
-edges
-str(edges)
+head(edges)
 
 # now make nodes anonymous by removing Name
 nodesanon = subset(nodes, select=-c(Name))
-nodesanon
-str(nodesanon)
+head(nodesanon)
 
 # rename unique id as Name
 nodesanon = rename(nodesanon, c("uniqueid"="Name"))
-nodesanon
-str(nodesanon)
+head(nodesanon)
 
 # make edges anonymous
 edgesanon = subset(edges, select=-c(Source, Target))
-edgesanon
-str(edgesanon)
+head(edgesanon)
 
 # rename to Source and Target
 edgesanon = rename(edgesanon, c("uniqueid.x"="Source", "uniqueid.y"="Target"))
-edgesanon
-str(edgesanon)
+head(edgesanon)
 
 # write out new NETWORK file
 write.xlsx(nodesanon, outfile, sheetName="Nodes",
